bd94df3851e4caab1cdb0ff0af16795f26f57941
UwU_Snek <xiaoju345@gmail.com>
2024-07-09 17:34:46 +0200

    Add staging tab Move documentation elements to the staging tab Add tab HTML tags Minor improvements

--- Scripts/Setup/DocumentationTabs.js
+++ Scripts/Setup/DocumentationTabs.js
@@ -13,21 +13,6 @@ let tab_button_internal;
 
 
 var setup_tabs = {
-    get_local_root : function(elm){
-        if(elm.parentNode.tagName === "MAIN-RIGHT-DOC-SCROLL-") return elm;
-        else return setup_tabs.get_local_root(elm.parentNode);
-    },
-
-
-    get_parent_header : function(elm){
-        if(elm.tagName == "H1") return elm;
-        else return setup_tabs.get_parent_header(elm.previousSibling);
-    },
-
-
-
-
-
 
     create_button : function(text, tab_num){
         // Create the button element
@@ -89,12 +74,12 @@ var setup_tabs = {
         /*
         // For each example element
         let elms = document.querySelectorAll(
-            "main-right-doc-scroll- :not(split-example-container-right-) > example-," +
-            "main-right-doc-scroll-                                      > example-"
+            "#main-right-staging :not(split-example-container-right-) > example-," +
+            "#main-right-staging                                      > example-"
         ); 
         for(let i = 0; i < elms.length; ++i) {
             let button = document.createElement("span");
-
+-
             // Find additional examples
             let local_root = setup_tabs.get_local_root(elms[i]);
             let parent_header = setup_tabs.get_parent_header(local_root);
@@ -112,7 +97,7 @@ var setup_tabs = {
 
                 // Add click listener
                 button.addEventListener("click", function() {
-                    document.getElementById("main-right-doc").style.marginLeft = "calc(0px - 100% - var(--main-padding-r))";
+                    document.getElementById("main-right-staging").style.marginLeft = "calc(0px - 100% - var(--main-padding-r))";
                     let target_container = document.querySelector("#main-right-examples > main-right-examples-scroll-");
                     for(let j = 0; j < examples.length; ++j) {
                         target_container.appendChild(examples[j]);
@@ -137,21 +122,45 @@ var setup_tabs = {
 
 
 
-    move_elements : function(class_name, output_list) {
-        // For each extra example
-        let elms = document.getElementsByClassName(class_name)
+
+    get_local_root : function(elm){
+        if(elm.parentNode.id === "main-right-staging") return elm;
+        else return setup_tabs.get_local_root(elm.parentNode);
+    },
+
+
+    get_parent_header : function(elm){
+        if(typeof elm != 'undefined' && elm != null) {
+            if(elm.tagName == "H1") return elm;
+            else return setup_tabs.get_parent_header(elm.previousSibling);
+        }
+        else return null;
+    },
+
+
+
+    move_elements : function(tag_name, output_list) {
+        // For each "moveto" tag
+        let elms = document.getElementsByTagName(tag_name)
         for(let i = 0; i < elms.length; ++i) {
 
             // Get it's header number
             let local_root = setup_tabs.get_local_root(elms[i]);
             let parent_header = setup_tabs.get_parent_header(local_root);
+            if(parent_header == null) continue;
             let header_number = (parent_header.innerHTML).match(/([0-9]+\.)+/g)[0];
 
-            // Save it in the example hash map and remove it from the page
+            // Create header element in the map if it doesnt exist yet
             if(!output_list.has(header_number)) {
                 output_list.set(header_number, new Array());
             }
-            output_list.get(header_number).push(elms[i]);
+
+            // Save each of its children in the hash map and remove them from the page, then remove the container
+            let c = elms[i].children;
+            for(let j = 0; j < c.length; ++j){
+                output_list.get(header_number).push(c[j]);
+                c[j].remove();
+            }
             elms[i].remove();
         }
     },
@@ -160,8 +169,9 @@ var setup_tabs = {
 
 
     init : function(){
-        setup_tabs.move_elements("moveto-example", example_list);
-        setup_tabs.move_elements("moveto-internal", internal_list);
+        setup_tabs.move_elements("moveto-doc-", example_list);
+        setup_tabs.move_elements("moveto-example-", example_list);
+        setup_tabs.move_elements("moveto-internal-", internal_list);
         setup_tabs.create_tab_buttons();
     }
 }
