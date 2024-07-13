0986216fc1960dc6604e6703dbd59139f9c37e96
UwU_Snek <xiaoju345@gmail.com>
2024-07-13 23:46:51 +0200

    Fix copy example button

--- Scripts/UI/CopyCode.js
+++ Scripts/UI/CopyCode.js
@@ -7,29 +7,30 @@ var ui_copy_code = {
         return txt.value;
     },
 
-    copy_code : function(lable) {
-        let children = lable.parentNode.childNodes;
-        for(let i = 0; i < children.length; i++){
-            if(children[i].tagName == "DIV") {
 
-                // Convert <br> to newlines and decode the lines one by one
-                let output_text = children[i].innerHTML.replaceAll(/<br>/g, '\n').split('\n');
-                for(let i = 0; i < output_text.length; i++) {
-                    output_text[i] = ui_copy_code.decodeHTML(output_text[i].replaceAll(/<\/?(!--|([a-zA-Z0-9_\-]+))[^>]+>/g, '')).slice(4);
-                }
-
-                // Join the lines and remove all leading and trailing newlines
-                navigator.clipboard.writeText(output_text.join('\n').trim());
-                return 0;
-            }
+    copy_code : function(label) {
+        // Convert <br> to newlines and decode the lines one by one
+        let output_text = label.nextElementSibling.innerHTML.replaceAll(/<br>/g, '\n').split('\n');
+        for(let i = 0; i < output_text.length; i++) {
+            output_text[i] = ui_copy_code.decodeHTML(
+                output_text[i]                                         // Base line output
+                .replaceAll(/<ce->.*?<\/ce->/g, "")                    // Remove error tags
+                .replaceAll(/<\/?(!--|([a-zA-Z0-9_\-]+))[^>]+>/g, '')) // Remove tags
+                .slice(4)                                              // Remove line number
+            ;
         }
+
+        // Join the lines, replace visible spaces with actual spaces and remove all leading and trailing newlines
+        navigator.clipboard.writeText(output_text.join('\n').replaceAll(/·/g, " ").trim());
     },
+
+
 
 
     init : function() {
         let children = document.querySelectorAll("example- > label-");
         for(let i = 0; i < children.length; i++){
-            children[i].addEventListener('click', function(){ ui_copy_code.copy_code(this); });
+            children[i].addEventListener('click', function(){ ui_copy_code.copy_code(children[i]); });
         }
     }
 }
