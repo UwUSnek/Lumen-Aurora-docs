3c17ee3df4075fcf74e749aef2420e5393ac5f0a
UwU_Snek <xiaoju345@gmail.com>
2024-07-14 20:09:04 +0200

    Rename main HTML file Rename generated output file

--- Build.sh
+++ Build.sh
@@ -3,12 +3,12 @@
 
 
 
-cat ./Page/Lux-Umbra-Languages.html \
+cat ./Page/Lumina-Aurora-Languages.html \
     | perl -ne "s/^\s*#include/#include/; print;"                                           `# Remove leading spaces for traditional cpp`               \
     | gcc -I. -E -P -CC -w -xc -traditional-cpp -                                                                                                       \
     | perl -0ne "s/^[\S\s]*?(?=<!DOCTYPE html>)//; print;"                                  `# Remove everything added by cpp before the !DOCTYPE tag`  \
     | perl -0ne "s/(\n(\s+)<!--[\S\s]*?-->)|(<!--[\S\s]*?-->)|(<!--[\S\s]*?$)//g; print;"   `# Remove all comments`                                     \
-> Lux-Umbra-Languages.html
+> Lumina-Aurora-Languages.html
 
 
 
