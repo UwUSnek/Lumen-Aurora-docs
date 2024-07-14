fe6e9457d77cd0b456567f7f2fd3c97b9a4fee3e
UwU_Snek <xiaoju345@gmail.com>
2024-07-15 01:31:52 +0200

    Fix example counter Add warning block CSS Rule Add a warning to include statements Improve line continuation token documentation

--- Scripts/Custom/CreateTabNames.js
+++ Scripts/Custom/CreateTabNames.js
@@ -2,7 +2,7 @@
 var create_tab_names = {
     start : function() {
         tab_button_doc.innerHTML = `Documentation`;
-        tab_button_examples.innerHTML = `Examples [${ tab_examples.querySelectorAll("example- label-").length }]`;
+        tab_button_examples.innerHTML = `Examples [${ tab_examples.querySelectorAll("example- label-").length - tab_examples.querySelectorAll("split-example-container-").length }]`;
         tab_button_internal.innerHTML = `Internal functioning${ (tab_internal.hasChildNodes() && tab_internal.childNodes[0].tagName != "TAB-PLACEHOLDER-") ? " [*]" : "" }`;
     }
 }
