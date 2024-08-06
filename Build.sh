76712db6bb9e0de8a0658e4a097bce076027fd01
UwU_Snek <xiaoju345@gmail.com>
2024-08-06 15:10:04 +0200

    Add license Remove "internal" page

--- Build.sh
+++ Build.sh
@@ -10,12 +10,3 @@ cat ./Page/Lumina-Aurora-Languages.html \
     | perl -0ne "s/(\n(\s+)<!--[\S\s]*?-->)|(<!--[\S\s]*?-->)|(<!--[\S\s]*?$)//g; print;"   `# Remove all comments`                                     \
 > Lumina-Aurora-Languages.html
 
-
-
-
-cat ./Page/Internal.html \
-    | perl -ne "s/^\s*#include/#include/; print;"                                           `# Remove leading spaces for traditional cpp`               \
-    | gcc -I. -E -P -CC -w -xc -traditional-cpp -                                                                                                       \
-    | perl -0ne "s/^[\S\s]*?(?=<!DOCTYPE html>)//; print;"                                  `# Remove everything added by cpp before the !DOCTYPE tag`  \
-    | perl -0ne "s/(\n(\s+)<!--[\S\s]*?-->)|(<!--[\S\s]*?-->)|(<!--[\S\s]*?$)//g; print;"   `# Remove all comments`                                     \
-> Internal.html
