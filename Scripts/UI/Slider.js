0a6fde0e0399a94191abd41b894f4fa6ba26ca7a
UwU_Snek <thatsnekuwu@gmail.com>
2026-02-14 02:50:27 +0100

    Fix language logos Other minor improvements

--- Scripts/UI/Slider.js
+++ Scripts/UI/Slider.js
@@ -24,22 +24,22 @@ var ui_slider = {
 
     // Updates the logo- X-position and height
     update_logos : function(){
-        
+
         // If one of more logo elements are present in the current documentation tab
         let logos = tab_doc.querySelectorAll("logo-");
         let page_width = document.documentElement.clientWidth;
         if(logos != null && logos.length > 0) {
 
             // For each logo element
-            for(let i = 0; i < logos.length; ++i){
+            for(const element of logos){
 
                 // Move the background image from left of the viewport to center of the element (X only)
-                logos[i].style.backgroundPositionX = `calc(${ (page_width + parseInt(slider.value) + main_padding_l_px) / 2 }px - ${ getComputedStyle(logos[i]).backgroundSize } / 2)`;
+                element.style.backgroundPositionX = `calc(${ (page_width + Number.parseInt(slider.value) + main_padding_l_px) / 2 }px - ${ getComputedStyle(element).backgroundSize } / 2)`;
 
                 // Make its height identical to its (dynamic) width  //! Only setting the height property with min-height at 0 doesn't work
-                let height = `min(60vh, ${ page_width - parseInt(slider.value) - main_padding_r_px - main_padding_l_px * 2 }px)`;
-                logos[i].style.minHeight = height;
-                logos[i].style.maxHeight = height;
+                let height = `min(20vh, ${ page_width - Number.parseInt(slider.value) - main_padding_r_px - main_padding_l_px * 2 }px)`;
+                element.style.minHeight = height;
+                element.style.maxHeight = height;
             }
         }
     },
@@ -49,12 +49,12 @@ var ui_slider = {
 
     // Updates the width of the left and right main containers
     update_main_width : function(){
-        console.log("value: " + parseInt(slider.value));
-        left.style.width  =             `${ Math.max(0, parseInt(slider.value) - main_padding_l_px) }px`;
-        right.style.width = `calc(100% - ${ Math.max(0, parseInt(slider.value) + main_padding_l_px + main_padding_r_px * 2) }px)`;
+        console.log("value: " + Number.parseInt(slider.value));
+        left.style.width  =             `${ Math.max(0, Number.parseInt(slider.value) - main_padding_l_px) }px`;
+        right.style.width = `calc(100% - ${ Math.max(0, Number.parseInt(slider.value) + main_padding_l_px + main_padding_r_px * 2) }px)`;
         //! ^ HTML Slider's minimum value doesn't actually work. The Math.max calls are used to prevent elements' widths from going negative
-        
-        window.localStorage.setItem("slider-value", slider.value);
+
+        globalThis.localStorage.setItem("slider-value", slider.value);
     },
 
 
@@ -67,18 +67,18 @@ var ui_slider = {
 
 
     init_slider_first_time : function(){
-        window.localStorage.setItem("slider-set", "set");
+        globalThis.localStorage.setItem("slider-set", "set");
         let min = main_padding_l_px;
         let max = document.documentElement.clientWidth - main_padding_r_px * 2 - slider_width_px;
-        window.localStorage.setItem("slider-value", (min + max) / 2);
+        globalThis.localStorage.setItem("slider-value", (min + max) / 2);
     },
     // Set slider value after page refresh and initialize it if needed
     init_slider : function(){
-        if(window.localStorage.getItem("slider-set") != "set") {
+        if(globalThis.localStorage.getItem("slider-set") != "set") {
             ui_slider.init_slider_first_time();
         }
         slider.addEventListener("mouseup", function(){ ui_slider.update_main_width(); ui_slider.update_logos() });
-        slider.value = window.localStorage.getItem("slider-value");
+        slider.value = globalThis.localStorage.getItem("slider-value");
     },
 
 
