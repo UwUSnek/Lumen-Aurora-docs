4448acb9b12aa66ed3d7f01109b49c20152afd3e
UwU_Snek <thatsnekuwu@gmail.com>
2026-03-26 20:10:47 +0100

    Fix height of tab container Fix external links not working Replace stacked footer layout with a more appropriate flex element Update language structure documentation

--- Scripts/UI/SmoothLinks.js
+++ Scripts/UI/SmoothLinks.js
@@ -1,11 +1,21 @@
 
+
 const ui_smooth_links = {
     init : function(){
         for(const link of document.getElementsByTagName("A")){
-            link.addEventListener("click", function(event){
-                event.preventDefault();
-                setup_index.go_to_index(event.target.href);
-            });
+
+            // Local links - Replace with a custom function call
+            if(link.getAttribute("href")?.startsWith("#")){
+                link.addEventListener("click", function(event){
+                    event.preventDefault();
+                    setup_index.go_to_index(event.target.href);
+                });
+            }
+
+            // External links - Open a new tab by default, instead of replacing the current one
+            else {
+                link.setAttribute("target", "_blank");
+            }
         }
     }
 }
